f8f986c6d4d77620ee4da0093b5dfeb5cb977c4e
kerta24 <kertahendrawan@gmail.com>
2022-10-17 21:53:27 +0800

    Membuat contoh widget container responsive, membuat contoh gridview dan listview responsiv sesuai dengan orientasi perangkat dan ukuran dari perangkat

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]);
+  runApp(HomePage());
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Landing(),
+    );
+  }
+}
+
+class Landing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final mediaQueryHeight = MediaQuery.of(context).size.height;
+    final mediaQueryWidth = MediaQuery.of(context).size.width;
+    final myAppBar = AppBar(
+      title: Text('Responsive'),
+    );
+    final bodyHeigt = mediaQueryHeight -
+        myAppBar.preferredSize.height -
+        MediaQuery.of(context).padding.top -
+        MediaQuery.of(context).padding.bottom;
+
+    final bool isLansCape =
+        MediaQuery.of(context).orientation == Orientation.landscape;
+
+    return MaterialApp(
+      home: Scaffold(
+          appBar: myAppBar,
+          body: Center(
+              //Jika Lanscape
+              child: (isLansCape)
+                  ? Column(
+                      children: [
+                        Container(
+                          color: Colors.red,
+                          width: mediaQueryWidth,
+                          height: mediaQueryHeight * 0.4,
+                        ),
+                        Container(
+                          // margin: EdgeInsets.all(40),
+                          height: mediaQueryHeight * 0.4,
+                          color: Colors.amber,
+                          child: GridView.builder(
+                              gridDelegate:
+                                  SliverGridDelegateWithFixedCrossAxisCount(
+                                      crossAxisCount: 2),
+                              itemCount: 20,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  leading: CircleAvatar(),
+                                  title: Text("Data Kamu"),
+                                );
+                              }),
+                        )
+                      ],
+                    )
+                  // Jika Potrait
+                  : Column(
+                      children: [
+                        Container(
+                          color: Colors.red,
+                          width: mediaQueryWidth,
+                          height: mediaQueryHeight * 0.3,
+                        ),
+                        Container(
+                          // margin: EdgeInsets.all(40),
+                          height: mediaQueryHeight * 0.6,
+                          color: Colors.amber,
+                          child: ListView.builder(
+                              itemCount: 100,
+                              itemBuilder: (context, index) {
+                                return ListTile(
+                                  leading: CircleAvatar(),
+                                  title: Text("Data Kamu"),
+                                );
+                              }),
+                        )
+                      ],
+                    ))),
+      //
+    );
+  }
+}
